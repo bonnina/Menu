ce7e51e88082d07f0a2d8b70a556baac3b7fa4fc
bonnina <37539513+bonnina@users.noreply.github.com>
2018-10-29 15:25:10 +0200

    Add Provider, store and logger

--- src/index.js
+++ src/index.js
@@ -3,10 +3,29 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { Provider } from 'react-redux';
+import reducer from './reducers'
+import logger from 'redux-logger'
+import { createStore, applyMiddleware } from 'redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
+const store = createStore(
+  reducer, {
+    order: [], 
+    details: {
+      location: '',
+      time: ''
+    }
+  },
+  applyMiddleware(logger)
+);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>, 
+document.getElementById('root'));
+
+
 serviceWorker.unregister();
+//  http://bit.ly/CRA-PWA
