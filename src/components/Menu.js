d4f893d131d9b459089efafac8717955a5b882b5
bonnina <37539513+bonnina@users.noreply.github.com>
2018-10-29 21:59:51 +0200

    Create Menu

---
+++ src/components/Menu.js
@@ -0,0 +1,79 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { withStyles } from '@material-ui/core/styles';
+import Drawer from '@material-ui/core/Drawer';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import List from '@material-ui/core/List';
+import Typography from '@material-ui/core/Typography';
+import Divider from '@material-ui/core/Divider';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import HomeIcon from '@material-ui/icons/Home';
+import LocalDining from '@material-ui/icons/LocalDining';
+import LocalCafe from '@material-ui/icons/LocalCafe';
+import ShoppingCart from '@material-ui/icons/ShoppingCart';
+import Delete from '@material-ui/icons/Delete';
+import styles from '../styles/menuStyles';
+
+
+const Menu = (props) => {
+  const { classes } = props;
+  
+  return (
+    <div className={classes.root}>
+      <CssBaseline />
+      <Drawer
+        className={classes.drawer}
+        variant="permanent"
+        classes={{
+          paper: classes.drawerPaper,
+        }}
+        anchor="left"
+      >
+        <div className={classes.toolbar} />
+        <List >
+          <ListItem button key='text'>
+          <Link to='/' style={{textDecoration: 'none'}}>
+            <ListItemIcon className={classes.listIcon}> 
+              <HomeIcon />
+            </ListItemIcon>
+            </Link>
+          </ListItem>
+        </List>
+        <Divider />
+        <List>
+        <ListItem button>
+            <ListItemIcon className={classes.listIcon}>
+              <LocalDining />
+            </ListItemIcon>
+          </ListItem>
+          <ListItem button>
+            <ListItemIcon className={classes.listIcon}>
+              <LocalCafe />
+            </ListItemIcon>
+          </ListItem>
+          <ListItem button >
+            <ListItemIcon className={classes.listIcon}> 
+              <ShoppingCart />
+            </ListItemIcon>
+          </ListItem>
+          <ListItem button >
+            <ListItemIcon className={classes.listIcon}> 
+              <Delete />
+            </ListItemIcon>
+          </ListItem>
+        </List>
+      </Drawer>
+      <main className={classes.content}>
+        <Typography variant='h3' className={classes.header} gutterBottom>
+          Click to add:
+        </Typography>
+      </main>
+    </div>
+  );
+}
+
+export default withStyles(styles)(
+  connect()(Menu)
+);
