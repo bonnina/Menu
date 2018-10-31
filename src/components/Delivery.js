eb7b3c70c76f8d9c3d47ded81a4d917232f4236f
bonnina <37539513+bonnina@users.noreply.github.com>
2018-10-31 18:05:27 +0200

    Update Delivery.js

--- src/components/Delivery.js
+++ src/components/Delivery.js
@@ -1,46 +1,95 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
+import { BrowserRouter as Router, Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import Typography from '@material-ui/core/Typography';
-import { withStyles } from '@material-ui/core/styles';
-import Menu from './Menu';
-import Food from './Food';
+ import { withStyles } from '@material-ui/core/styles';
+import { withRouter } from 'react-router-dom';
+import Drawer from '@material-ui/core/Drawer';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import List from '@material-ui/core/List';
+import Divider from '@material-ui/core/Divider';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import HomeIcon from '@material-ui/icons/Home';
+import LocalDining from '@material-ui/icons/LocalDining';
+import LocalCafe from '@material-ui/icons/LocalCafe';
+import ShoppingCart from '@material-ui/icons/ShoppingCart';
+import Delete from '@material-ui/icons/Delete';
+import styles from '../styles/menuStyles';
+import Items from './Items';
 
-const styles = theme => ({
-  root: {
-    display: 'flex',
-  },
-  header: {
-    color: '#b3b3b3',
-    [theme.breakpoints.down('sm')]: {
-      fontSize: '40px'
-    },
-    [theme.breakpoints.down('xs')]: {
-      fontSize: '30px'
-    }
-  },
-  content: {
-    flexGrow: 1,
-    backgroundColor: '',
-    padding: theme.spacing.unit * 3,
-  },
-});
+class Delivery extends React.Component {
 
-const Delivery = (props) => {
-  const { classes } = props;
+  render() {
+  const { classes, history, match } = this.props;
   
   return (
-    <div className={classes.root}>
-    <Menu />
-    <main className={classes.content}>
-      <Typography variant='h3' className={classes.header} gutterBottom>
+    <Router >
+    <div className={classes.root}> 
+      <div>
+      <CssBaseline />
+      <Drawer
+        className={classes.drawer}
+        variant="permanent"
+        classes={{
+          paper: classes.drawerPaper,
+        }}
+        anchor="left"
+      >
+        <div className={classes.toolbar} />
+        <List >
+          <ListItem button key='text'>
+          <Link to='/' style={{textDecoration: 'none'}} onClick={() => {history.push('/')}}>
+            <ListItemIcon className={classes.listIcon}> 
+              <HomeIcon />
+            </ListItemIcon>
+          </Link>
+          </ListItem>
+        </List>
+        <Divider />
+        <List>
+        <ListItem button>
+          <Link to={`${match.url}/food`} style={{textDecoration: 'none'}}> 
+            <ListItemIcon className={classes.listIcon}>
+              <LocalDining />
+            </ListItemIcon>
+          </Link> 
+          </ListItem>
+          <ListItem button>
+            <Link to={`${match.url}/drinks`} style={{textDecoration: 'none'}}> 
+              <ListItemIcon className={classes.listIcon}>
+                <LocalCafe />
+              </ListItemIcon>
+            </Link> 
+          </ListItem>
+          <ListItem button >
+            <ListItemIcon className={classes.listIcon}> 
+              <ShoppingCart />
+            </ListItemIcon>
+          </ListItem>
+          <ListItem button >
+            <ListItemIcon className={classes.listIcon}> 
+              <Delete />
+            </ListItemIcon>
+          </ListItem>
+        </List>
+      </Drawer>
+    </div>
+      
+      <main className={classes.content}> 
+      <Typography variant='h3' gutterBottom>
         Click to add:
       </Typography>
-      <Food />
+
+      <Route path={`${match.path}/:name`} component={Items}/>
     </main>
     </div>
+    </Router>
   );
+  }
 }
 
-export default withStyles(styles)(
-  connect()(Delivery)
+export default  withStyles(styles)(
+  withRouter( connect()(Delivery) )
 );
