17742cb36c25fe615a4eea8eaa6e510afe782116
bonnina <37539513+bonnina@users.noreply.github.com>
2018-10-31 18:13:37 +0200

    Update: removed all routes except for Home and Delivery

--- src/App.js
+++ src/App.js
@@ -3,8 +3,6 @@ import { BrowserRouter, Route } from 'react-router-dom';
 import './App.css';
 import Home from './components/Home';
 import Delivery from './components/Delivery';
-import Menu from './components/Menu';
-import Food from './components/Food';
 import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 
 const THEME = createMuiTheme({
@@ -20,10 +18,8 @@ class App extends Component {
         <MuiThemeProvider theme={THEME}>
           <BrowserRouter>
           <div>
-            <Route exact path="/" render={(props) => <Home {...props} />} />
-            <Route exact path="/menu" render={(props) => <Menu {...props} />} />
+            <Route exact path='/' render={(props) => <Home {...props} />} />
             <Route exact path="/delivery" render={(props) => <Delivery {...props} />} />
-            <Route exact path="/food" render={(props) => <Food {...props} />} />
           </div>
           </BrowserRouter>
         </MuiThemeProvider>
