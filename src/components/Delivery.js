48f161e02b268f6ae341b6d1a7f5eb8c765c331e
bonnina <37539513+bonnina@users.noreply.github.com>
2018-11-01 08:47:34 +0200

    Update: (rollback) enebled importing Menu as a child component

--- src/components/Delivery.js
+++ src/components/Delivery.js
@@ -1,92 +1,32 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
-import { BrowserRouter as Router, Link } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
 import Typography from '@material-ui/core/Typography';
- import { withStyles } from '@material-ui/core/styles';
+import { withStyles } from '@material-ui/core/styles';
 import { withRouter } from 'react-router-dom';
-import Drawer from '@material-ui/core/Drawer';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import List from '@material-ui/core/List';
-import Divider from '@material-ui/core/Divider';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import HomeIcon from '@material-ui/icons/Home';
-import LocalDining from '@material-ui/icons/LocalDining';
-import LocalCafe from '@material-ui/icons/LocalCafe';
-import ShoppingCart from '@material-ui/icons/ShoppingCart';
-import Delete from '@material-ui/icons/Delete';
-import styles from '../styles/menuStyles';
+import styles from '../styles/deliveryStyles';
 import Items from './Items';
+import Menu from './Menu';
 
 class Delivery extends React.Component {
-
   render() {
   const { classes, history, match } = this.props;
   
   return (
-    <Router >
-    <div className={classes.root}> 
-      <div>
-      <CssBaseline />
-      <Drawer
-        className={classes.drawer}
-        variant="permanent"
-        classes={{
-          paper: classes.drawerPaper,
-        }}
-        anchor="left"
-      >
-        <div className={classes.toolbar} />
-        <List >
-          <ListItem button key='text'>
-          <Link to='/' style={{textDecoration: 'none'}} onClick={() => {history.push('/')}}>
-            <ListItemIcon className={classes.listIcon}> 
-              <HomeIcon />
-            </ListItemIcon>
-          </Link>
-          </ListItem>
-        </List>
-        <Divider />
-        <List>
-        <ListItem button>
-          <Link to={`${match.url}/food`} style={{textDecoration: 'none'}}> 
-            <ListItemIcon className={classes.listIcon}>
-              <LocalDining />
-            </ListItemIcon>
-          </Link> 
-          </ListItem>
-          <ListItem button>
-            <Link to={`${match.url}/drinks`} style={{textDecoration: 'none'}}> 
-              <ListItemIcon className={classes.listIcon}>
-                <LocalCafe />
-              </ListItemIcon>
-            </Link> 
-          </ListItem>
-          <ListItem button >
-            <ListItemIcon className={classes.listIcon}> 
-              <ShoppingCart />
-            </ListItemIcon>
-          </ListItem>
-          <ListItem button >
-            <ListItemIcon className={classes.listIcon}> 
-              <Delete />
-            </ListItemIcon>
-          </ListItem>
-        </List>
-      </Drawer>
-    </div>
-      
-      <main className={classes.content}> 
-      <Typography variant='h3' gutterBottom>
-        Click to add:
-      </Typography>
+    <BrowserRouter>
+      <div className={classes.root}> 
+        <Menu match={match} history={history} />
+        <main className={classes.content}> 
+          <Typography variant='h3'  className={classes.header} gutterBottom>
+            Click to add:
+          </Typography>
 
-      <Route path={`${match.path}/:name`} component={Items}/>
-    </main>
-    </div>
-    </Router>
-  );
+          <Route path={`${match.path}/:name`} component={Items}/>
+        </main>
+      </div>
+    </BrowserRouter>
+    );
   }
 }
 
