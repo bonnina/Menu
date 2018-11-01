5bbe1d6de3fd5268dce9bfb455908fab022d6669
bonnina <37539513+bonnina@users.noreply.github.com>
2018-11-01 15:06:23 +0200

    Edit: moved Appbar styles to AppBarStyles

--- src/styles/homepage.js
+++ src/styles/homepage.js
@@ -105,16 +105,6 @@ const styles = theme => ({
         marginLeft: "-1vw",
       }
     },
-    logoSmall: {
-      color: '#cc6666',
-      fontSize: '25px',
-      lineHeight: 0.6,
-      fontWeight: 'bold',
-      marginTop: '-1vw',
-      '&:hover': {
-        // transform: 'translate(2px, 1px) rotate(270deg)'
-      }
-    },
     card: {
       height: '40vw',
       width: '30vw',
@@ -167,52 +157,6 @@ const styles = theme => ({
         width: '23vh'
       }
     },
-    appBar: {
-      top: 'auto',
-      bottom: 0,
-      backgroundColor: '#f6eeee',
-      height: '2.8vw',
-      [theme.breakpoints.down('sm')]: {
-        height: '7vw',
-      },
-      [theme.breakpoints.down('xs')]: {
-        height: '10vw',
-      }
-    },
-    toolbar: {
-      alignItems: 'center',
-      justifyContent: 'space-between',
-    },
-    iconBut: {
-      '&:hover': {
-        backgroundColor: '#f6eeee', 
-        color: '#993333'
-      }
-    },
-    iconCart: {
-      color: '#a37575',
-      fontSize: '35px',
-      marginTop: '-1vw',
-      '&:hover': {
-        color: '#993333'
-      }
-    },
-    iconMenu: {
-      color: '#a37575',
-      fontSize: '35px',
-      marginTop: '-1vw',
-      '&:active': {
-        backgroundColor: '#f6eeee', 
-        color: '#993333'
-      }
-    },
-    tooltip: {
-      fontSize: '20px',
-      '&:hover': {
-        backgroundColor: '#f6eeee', 
-        color: '#993333',
-      },
-    }
   });
 
   export default styles;
