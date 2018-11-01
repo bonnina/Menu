cd4120893a236ef418db7c47befbe485430a6a8c
bonnina <37539513+bonnina@users.noreply.github.com>
2018-11-01 20:14:33 +0200

    Update heading styles

--- src/styles/deliveryStyles.js
+++ src/styles/deliveryStyles.js
@@ -3,18 +3,26 @@ const styles = theme => ({
     display: 'flex',
   },
   header: {
-    color: '#b3b3b3',
+    color: '#d9d9d9',
+    marginBottom: '30px',
+    marginLeft: '10px',
     [theme.breakpoints.down('sm')]: {
       fontSize: '40px'
     },
     [theme.breakpoints.down('xs')]: {
-      fontSize: '30px'
-    }
+      fontSize: '30px',
+      marginLeft: '-10px',
+    },
+    [theme.breakpoints.down('365')]: {
+      fontSize: '25px',
+      marginLeft: '-15px',
+    },
   },
   content: {
     flexGrow: 1,
     backgroundColor: 'white',
     padding: theme.spacing.unit * 3,
+    height: '100vh'
   },
 });
 
