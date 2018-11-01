2933ac338313f6595bf324aef6f8cfb2175f8e4b
bonnina <37539513+bonnina@users.noreply.github.com>
2018-11-01 20:21:05 +0200

    Update Items: add grid, move items to itemStyles

--- src/components/Items.js
+++ src/components/Items.js
@@ -1,28 +1,33 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { withStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
+import { styles, items} from '../styles/itemStyles';
 
-const items = [
-  {
-    name: 'food',
-    contents: []
-  },
-  {
-    name: 'drinks',
-    contents: []
-  }
-];
-
-const Items = ({ match }) => {
-
+class Items extends React.Component {
+  render() {
+  const { classes, match } = this.props;
   const item = items.find(({ name }) => name === match.params.name);
 
-  return (
-    <div>
-       <Typography variant='h2' gutterBottom>
-        {item.name}
-      </Typography>
-    </div>
-  )
+    return (
+      <div className={classes.root}>
+        <Grid container spacing={8} justify="center" className={classes.container}>
+        {item.contents.map((el) => (
+          <Grid item key={el.text} style={{width: `${el.space}`}} >
+              <Typography variant="h2" style={{
+                backgroundColor: `${el.backgroundColor}`,
+              }} className={classes.text}>
+              {el.text}
+              </Typography>
+          </Grid>
+        ))}
+        </Grid>
+      </div>
+    )
+  }
 };
 
-export default Items;
+export default withStyles(styles)(
+  connect()(Items) 
+);
