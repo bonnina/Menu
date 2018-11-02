3870ea63fb5bb25553f4a5c431ce474b4a3f7d79
bonnina <37539513+bonnina@users.noreply.github.com>
2018-11-02 13:08:08 +0200

    Add temporary property display

--- src/components/food_and_drinks.js
+++ src/components/food_and_drinks.js
@@ -6,107 +6,128 @@ export const items = [
       {
         text: 'eggs',
         backgroundColor: '#ffff99',
-        space: '3.4em'
+        space: '3.4em',
+        display: ''
       },
       {
         text: 'bacon',
         backgroundColor: '#dfbf9f',
-        space: '4.1em'
+        space: '4.1em',
+        display: ''
       },
       {
         text: 'strawberries',
         backgroundColor: '#fed9cd',
-        space: '7.8'
+        space: '7.8',
+        display: ''
       },
       {
         text: 'apple',
         backgroundColor: '#dbfa9e',
-        space: '4.7'
+        space: '4.7',
+        display: ''
       },
       {
         text: 'toast',
         backgroundColor: '#ecd9c6',
-        space: '3.8em'
+        space: '3.8em',
+        display: ''
       },
       {
         text: 'cereal',
         backgroundColor: '#e5f2ff',
-        space: '4.2em'
+        space: '4.2em',
+        display: ''
       },
       {
         text: 'cheese',
         backgroundColor: '#ffeb99',
-        space: '4.5em'
+        space: '4.5em',
+        display: ''
       },
       {
         text: 'grapes',
         backgroundColor: '#e5ccff',
-        space: '4.4em'
+        space: '4.4em',
+        display: ''
       },
       {
         text: 'avocado',
         backgroundColor: '#c1f0c1',
-        space: '5.2em'
+        space: '5.2em',
+        display: ''
       },
       {
         text: 'cookies',
         backgroundColor: '#ffffb3',
-        space: '4.7em'
+        space: '4.7em',
+        display: ''
       },
       {
         text: 'honey',
         backgroundColor: '#fff0b3',
-        space: '4.1em'
+        space: '4.1em',
+        display: ''
       },
       {
         text: 'rice',
         backgroundColor: '#ebebe0',
-        space: '3em'
+        space: '3em',
+        display: ''
       },
       {
         text: 'jam',
         backgroundColor: '#ffcccc',
-        space: '2.8em'
+        space: '2.8em',
+        display: ''
       },
       {
         text: 'spinach',
         backgroundColor: '#99e699',
-        space: '4.9em'
+        space: '4.9em',
+        display: ''
       },
       {
         text: 'banana',
         backgroundColor: '#fcfc9c',
-        space: '4.7em'
+        space: '4.7em',
+        display: ''
       },
       {
         text: 'tomato',
         backgroundColor: '#f7baba',
-        space: '4.7em'
+        space: '4.7em',
+        display: ''
       },
       {
         text: 'grilled salmon',
         backgroundColor: '#e8c9c9',
-        space: '8em'
+        space: '8em',
+        display: ''
       },
       {
         text: 'pancakes',
         backgroundColor: '#ffeecc',
-        space: '5.5em'
+        space: '5.5em',
+        display: ''
       },
       { 
         text: 'pear',
         backgroundColor: '#d5ff80',
-        space: '3.5em'
+        space: '3.5em',
+        display: ''
       },
       {
         text: 'ham',
         backgroundColor: '#ecd9c6',
-        space: '3.3em'
+        space: '3.3em',
+        display: ''
       },
       {
         text: 'fries',
         backgroundColor: '#f5f589',
-        space: '4.4em'
+        space: '4.4em',
+        display: ''
       },
     ]
   },
@@ -116,57 +137,68 @@ export const items = [
       {
         text: 'water',
         backgroundColor: '#cceeff',
-        space: '4em'
+        space: '4em',
+        display: ''
       },
       {
         text: 'coffee',
         backgroundColor: '#d9b38c',
-        space: '4em'
+        space: '4em',
+        display: ''
       },
       {
         text: 'milk',
         backgroundColor: '#e0e0eb',
-        space: '3.4em'
+        space: '3.4em',
+        display: ''
       },
       {
         text: 'tea',
         backgroundColor: '#c2d6c2',
-        space: '3em'
+        space: '3em',
+        display: ''
       },
       {
         text: 'orange juice',
         backgroundColor: '#ffa366',
-        space: '7em'
+        space: '7em',
+        display: ''
       },
       {
         text: 'apple juice',
         backgroundColor: '#abedab',
-        space: '6.7em'
+        space: '6.7em',
+        display: ''
       },
       {
         text: 'cocoa',
         backgroundColor: '#c2a3a3',
-        space: '4em'
+        space: '4em',
+        display: ''
       },
       {
         text: 'almond milk',
         backgroundColor: '#ffddcc',
-        space: '6.8em'
+        space: '6.8em',
+        display: ''
       },
       {
         text: 'yogurt',
         backgroundColor: '#ffe6ff',
-        space: '4.4em'
+        space: '4.4em',
+        display: ''
       },
       {
         text: 'smothie',
         backgroundColor: '#f2ccff',
-        space: '5em'
+        space: '5em',
+        display: ''
       },
       {
         text: 'hot chocolate',
         backgroundColor: '#e0a385',
-        space: '7.5em'
+        space: '7.5em',
+        display: ''
       },
     ]
   }
