06a8178fd93d8661636c0a5428d6cbe4e284500e
bonnina <37539513+bonnina@users.noreply.github.com>
2018-11-05 10:05:00 +0200

    Add WebFont loader postpone initiation of app until fonts are active

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { Provider } from 'react-redux';
 import reducer from './reducers'
 import logger from 'redux-logger'
 import { createStore, applyMiddleware } from 'redux';
-
+import WebFont from 'webfontloader';
 
 const store = createStore(
   reducer, {
@@ -20,12 +20,26 @@ const store = createStore(
   applyMiddleware(logger)
 );
 
+const app = () => {
 ReactDOM.render(
   <Provider store={store}>
     <App />
   </Provider>, 
 document.getElementById('root'));
+};
 
+// postpone initiation of app until fonts are active
+const webFontConfig = {
+  google: {
+      families: ['Fredoka One'],
+  },
+  classes: false,
+  timeout: 1000,
+  active: app, 
+};
+
+
+WebFont.load(webFontConfig);
 
 serviceWorker.unregister();
 //  http://bit.ly/CRA-PWA
