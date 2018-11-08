457c9da85c366e0c2b36afdf4223ad829e8e20ec
bonnina <37539513+bonnina@users.noreply.github.com>
2018-11-08 14:10:27 +0200

    Remove unneeded comments

--- src/styles/homepage.js
+++ src/styles/homepage.js
@@ -1,5 +1,3 @@
-// <div>Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Bread">Bread</a> from <a href="https://www.flaticon.com/"     title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/"     title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
-
 const styles = theme => ({
     container: {
        padding: '0 4vw 0 4vw',
